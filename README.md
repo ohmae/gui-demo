d2c256739786a76c17e9bbefecd918563645cf4f
OHMAE Ryosuke <ryo@mm2d.net>
2016-07-11 20:02:14 +0900

    SDK更新

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Android GUI Demo
 This is a simple Demo App of Android GUI.
 
+## Install
+[Google Play](https://play.google.com/store/apps/details?id=net.mm2d.guidemo)
+
 ## Author
 大前 良介 (OHMAE Ryosuke)
 http://www.mm2d.net/
--- app/src/main/java/net/mm2d/guidemo/navi/NavigationDrawerBlurActivity.java
+++ app/src/main/java/net/mm2d/guidemo/navi/NavigationDrawerBlurActivity.java
@@ -25,7 +25,6 @@ import android.support.v4.widget.DrawerLayout.DrawerListener;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -248,6 +247,34 @@ public class NavigationDrawerBlurActivity extends AppCompatActivity
             return 0;
         }
 
+        /**
+         * Stack Blur v1.0 from
+         * http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html
+         * Java Author: Mario Klingemann <mario at quasimondo.com>
+         * http://incubator.quasimondo.com
+         *
+         * created Feburary 29, 2004
+         * Android port : Yahel Bouaziz <yahel at kayenko.com>
+         * http://www.kayenko.com
+         * ported april 5th, 2012
+         *
+         * This is a compromise between Gaussian Blur and Box blur
+         * It creates much better looking blurs than Box Blur, but is
+         * 7x faster than my Gaussian Blur implementation.
+         *
+         * I called it Stack Blur because this describes best how this
+         * filter works internally: it creates a kind of moving stack
+         * of colors whilst scanning through the image. Thereby it
+         * just has to add one new block of color to the right side
+         * of the stack and remove the leftmost color. The remaining
+         * colors on the topmost layer of the stack are either added on
+         * or reduced by one, depending on if they are on the right or
+         * on the left side of the stack.
+         *
+         * If you are using this algorithm in your code please add
+         * the following line:
+         * Stack Blur Algorithm by Mario Klingemann <mario@quasimondo.com>
+         */
         public Bitmap fastBlur(Bitmap sentBitmap, int radius, int color) {
             Bitmap bitmap = Bitmap.createBitmap(sentBitmap);
             if (radius < 1) {
@@ -256,7 +283,6 @@ public class NavigationDrawerBlurActivity extends AppCompatActivity
             int w = bitmap.getWidth();
             int h = bitmap.getHeight();
             int[] pix = new int[w * h];
-            Log.e("pix", w + " " + h + " " + pix.length);
             bitmap.getPixels(pix, 0, w, 0, 0, w, h);
             int wm = w - 1;
             int hm = h - 1;
